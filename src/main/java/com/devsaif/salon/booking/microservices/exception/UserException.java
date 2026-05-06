c0c5b492c926163481390719184b572e1905634a
SaifulAziz <gamestopik712@gmail.com>
2026-05-06 22:32:13 +0500

    Add Global Exception for (user not found)

---
+++ src/main/java/com/devsaif/salon/booking/microservices/exception/UserException.java
@@ -0,0 +1,7 @@
+package com.devsaif.salon.booking.microservices.exception;
+
+public class UserException extends Exception{
+    public UserException(String message){
+        super(message);
+    }
+}
