906eb5851e2f258e9c53446dfd49e717aae35771
SaifulAziz <gamestopik712@gmail.com>
2026-05-06 23:07:05 +0500

    Implimented UserService inteface for business logic

--- src/main/java/com/devsaif/salon/booking/microservices/service/impl/UserServiceImpl.java
+++ src/main/java/com/devsaif/salon/booking/microservices/service/impl/UserServiceImpl.java
@@ -1,4 +1,70 @@
 package com.devsaif.salon.booking.microservices.service.impl;
 
-public class UserServiceImpl {
+import com.devsaif.salon.booking.microservices.exception.UserException;
+import com.devsaif.salon.booking.microservices.model.User;
+import com.devsaif.salon.booking.microservices.repository.UserRepository;
+import com.devsaif.salon.booking.microservices.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepo;
+
+    @Override
+    public User createUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) throws UserException {
+        Optional<User> user = userRepo.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new UserException("user not found");
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public void deleteUserById(Long id) throws UserException {
+        Optional<User> user = userRepo.findById(id);
+        if (user.isEmpty()){
+            throw new UserException("user not found!!");
+
+        }
+        userRepo.deleteById(user.get().getId());
+    }
+
+    @Override
+    public void deleteAllUsers() {
+        userRepo.deleteAll();
+    }
+
+    @Override
+    public User updateUser(Long id, User user) throws UserException {
+        Optional<User> findUser = userRepo.findById(id);
+        if(findUser.isEmpty()){
+            throw new UserException("user not found!");
+        }
+        User existingUser = findUser.get();
+
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPhone(user.getPhone());
+        existingUser.setUpdatedAt(LocalDateTime.now());
+        existingUser.setRole(user.getRole());
+
+        return userRepo.save(existingUser);
+    }
 }
