02d3b4af336f2dcea8f0df189a3750240d17267e
SaifulAziz <gamestopik712@gmail.com>
2026-05-06 23:08:03 +0500

    Refactor All endpoints logic using UserService

--- src/main/java/com/devsaif/salon/booking/microservices/controller/UserController.java
+++ src/main/java/com/devsaif/salon/booking/microservices/controller/UserController.java
@@ -1,79 +1,59 @@
 package com.devsaif.salon.booking.microservices.controller;
 
 
-import com.devsaif.salon.booking.microservices.exception.UserException;
 import com.devsaif.salon.booking.microservices.model.User;
-import com.devsaif.salon.booking.microservices.repository.UserRepository;
+import com.devsaif.salon.booking.microservices.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController()
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepo;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody @Valid User user){
-        return userRepo.save(user);
+    public ResponseEntity<User> createUser(@RequestBody @Valid User user){
+        User createUser = userService.createUser(user);
+        return new ResponseEntity<>(createUser, HttpStatus.CREATED);
     }
 
     @GetMapping("/getusers")
-    public List<User> getUsers(){
-        return userRepo.findAll();
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable Long id) throws Exception {
-        Optional<User> user = userRepo.findById(id);
-        if(user.isPresent()){
-            return user.get();
-        }
-        throw new UserException("user not found");
+    public ResponseEntity<User> getUserById(@PathVariable Long id) throws Exception {
+        User getById= userService.getUserById(id);
+        return new ResponseEntity<>(getById, HttpStatus.OK);
     }
 
     @PutMapping("/users/{id}")
-    public User updateUser(@RequestBody User user,
+    public ResponseEntity<User> updateUser(@RequestBody User user,
                            @PathVariable Long id) throws Exception{
+        User updatedUser = userService.updateUser(id, user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
 
-        Optional<User> findUser = userRepo.findById(id);
-        if(findUser.isEmpty()){
-            throw new UserException("user not found!");
-        }
-        User existingUser = findUser.get();
-
-        existingUser.setFullName(user.getFullName());
-        existingUser.setEmail(user.getEmail());
-        existingUser.setPhone(user.getPhone());
-        existingUser.setUpdatedAt(LocalDateTime.now());
-        existingUser.setRole(user.getRole());
-
-        return userRepo.save(existingUser);
     }
 
     @DeleteMapping("/deleteAll")
     public void deleteAll(){
-        userRepo.deleteAll();
     }
 
     @DeleteMapping("/deleteById/{id}")
-    public String deleteById(@PathVariable Long id) throws Exception{
-        Optional<User> user = userRepo.findById(id);
-        if (user.isEmpty()){
-            throw new UserException("user not found!!");
-
-        }
-        userRepo.deleteById(user.get().getId());
-        return "user deleted";
-
+    public ResponseEntity<String> deleteById(@PathVariable Long id) throws Exception{
+        userService.deleteUserById(id);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.ACCEPTED);
     }
 
    }
