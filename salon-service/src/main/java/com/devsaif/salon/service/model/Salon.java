7227458592937080af999e15e390b7410bd9491d
SaifulAziz <gamestopik712@gmail.com>
2026-05-07 12:05:23 +0500

    Add salon model defined attributes

--- salon-service/src/main/java/com/devsaif/salon/service/model/Salon.java
+++ salon-service/src/main/java/com/devsaif/salon/service/model/Salon.java
@@ -1,4 +1,40 @@
 package com.devsaif.salon.service.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
 public class Salon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false)
+    private LocalDateTime openingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime ClosingTime;
 }
