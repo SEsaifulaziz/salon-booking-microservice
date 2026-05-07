2ccfed6905416ac60f8133a2370249df7d7da135
SaifulAziz <gamestopik712@gmail.com>
2026-05-07 12:32:00 +0500

    Add SalonService Interface

--- salon-service/src/main/java/com/devsaif/salon/service/service/SalonService.java
+++ salon-service/src/main/java/com/devsaif/salon/service/service/SalonService.java
@@ -1,4 +1,28 @@
 package com.devsaif.salon.service.service;
 
-public class SalonService {
+import com.devsaif.salon.service.model.Salon;
+import com.devsaif.salon.service.paload.dto.SalonDto;
+import com.devsaif.salon.service.paload.dto.UserDto;
+
+import java.util.List;
+
+public interface SalonService {
+
+    Salon createSalon(SalonDto salon, UserDto user);
+
+    Salon updateSalon(SalonDto salon, UserDto user,  Long salonId);
+
+    List<Salon> getSalons();
+
+    Salon getSalonById(Long id);
+
+    Salon DeleteAll();
+
+    Salon DeleteById(Long id);
+
+    Salon getSalonByOwnerId(Long ownerId);
+
+    List<Salon> searchSalonByCityName(String cityName);
+
+
 }
