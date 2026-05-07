3aff01c0c7c4f76fd17e3786bb9c6edf40242f86
SaifulAziz <gamestopik712@gmail.com>
2026-05-07 21:44:02 +0500

    Create PostMapping endpoint

--- salon-service/src/main/java/com/devsaif/salon/service/controller/SalonController.java
+++ salon-service/src/main/java/com/devsaif/salon/service/controller/SalonController.java
@@ -1,4 +1,34 @@
 package com.devsaif.salon.service.controller;
 
+import com.devsaif.salon.service.mapper.SalonMapper;
+import com.devsaif.salon.service.model.Salon;
+import com.devsaif.salon.service.paload.dto.SalonDto;
+import com.devsaif.salon.service.paload.dto.UserDto;
+import com.devsaif.salon.service.service.SalonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class SalonController {
+
+    private final SalonService salonService;
+
+    @PostMapping
+    public ResponseEntity<SalonDto> createSalon(@RequestBody SalonDto salonDto) {
+        UserDto userDto = new UserDto();
+        userDto.setId(1L);
+        Salon salon = salonService.createSalon(salonDto, userDto);
+        SalonDto salonDto1 = SalonMapper.mapToDto(salon);
+        return new ResponseEntity<>(salonDto1, HttpStatus.OK);
+    }
+
+
+
+
+
+
 }
