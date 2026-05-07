dae4c961aef2d5700b019176875477eac9c54705
SaifulAziz <gamestopik712@gmail.com>
2026-05-07 13:23:55 +0500

    Add Email attribute

--- salon-service/src/main/java/com/devsaif/salon/service/model/Salon.java
+++ salon-service/src/main/java/com/devsaif/salon/service/model/Salon.java
@@ -1,6 +1,7 @@
 package com.devsaif.salon.service.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -22,6 +23,10 @@ public class Salon {
 
     @Column(nullable = false)
     private String address;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
 
     @Column(nullable = false)
     private String phoneNumber;
