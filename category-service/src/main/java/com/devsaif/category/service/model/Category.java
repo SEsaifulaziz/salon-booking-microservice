95d72b914b55f58c40edd424f110aa289f4d7be9
SaifulAziz <gamestopik712@gmail.com>
2026-05-09 01:06:19 +0500

    Add Model defined attributes

--- category-service/src/main/java/com/devsaif/category/service/model/Category.java
+++ category-service/src/main/java/com/devsaif/category/service/model/Category.java
@@ -1,4 +1,22 @@
 package com.devsaif.category.service.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long salonId;
+
+    private String image;
+
 }
