6dacb67d66c5ebf803f53f15b6652dff301ee55e
SaifulAziz <gamestopik712@gmail.com>
2026-05-09 01:14:50 +0500

    Add Service interface

--- category-service/src/main/java/com/devsaif/category/service/service/CategoryService.java
+++ category-service/src/main/java/com/devsaif/category/service/service/CategoryService.java
@@ -1,4 +1,22 @@
 package com.devsaif.category.service.service;
 
-public class CategoryService {
+import com.devsaif.category.service.dto.SalonDto;
+import com.devsaif.category.service.model.Category;
+
+import java.util.List;
+import java.util.Set;
+
+public interface CategoryService {
+
+    Category createCategory(Category category, SalonDto salonDto);
+
+    Category updateCategory(Category category, SalonDto salonDto);
+
+    Set<Category> getAllCategoriesBySalonId(Long id);
+
+    Category getCategoryById(Long categoryId);
+
+    void deleteCategory(Long id);
+
+    void deleteAllCategories();
 }
