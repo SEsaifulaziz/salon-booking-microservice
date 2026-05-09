5bc49ca851f5b3374740a5fb19c2f0a3e25027c2
SaifulAziz <gamestopik712@gmail.com>
2026-05-09 13:18:35 +0500

    Add createCategory endpoint

--- category-service/src/main/java/com/devsaif/category/service/controller/SalonCategoryController.java
+++ category-service/src/main/java/com/devsaif/category/service/controller/SalonCategoryController.java
@@ -1,4 +1,28 @@
 package com.devsaif.category.service.controller;
 
+import com.devsaif.category.service.dto.SalonDto;
+import com.devsaif.category.service.model.Category;
+import com.devsaif.category.service.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categories/salon-owner")
+@RequiredArgsConstructor
 public class SalonCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) throws Exception{
+        SalonDto salonDto = new SalonDto();
+        salonDto.setId(1L);
+
+        Category savedCategory = categoryService.createCategory(category,salonDto);
+        return ResponseEntity.ok().body(savedCategory);
+    }
+
+
+
 }
