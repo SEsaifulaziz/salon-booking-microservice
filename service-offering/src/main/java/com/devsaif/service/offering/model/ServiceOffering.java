6d5aae164ced4917c1775bd0e16de689b51df677
SaifulAziz <gamestopik712@gmail.com>
2026-05-09 14:23:06 +0500

    Add Model defined attributes

---
+++ service-offering/src/main/java/com/devsaif/service/offering/model/ServiceOffering.java
@@ -0,0 +1,36 @@
+package com.devsaif.service.offering.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ServiceOffering {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private Long salonId;
+
+    @Column(nullable = false)
+    private Long categoryId;
+
+    private String image;
+
+
+
+}
