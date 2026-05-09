5f14d4e6957e22170c643ecc133f80989340b382
SaifulAziz <gamestopik712@gmail.com>
2026-05-09 13:03:48 +0500

    Add CategoryController

---
+++ category-service/src/main/java/com/devsaif/category/service/controller/CategoryController.java
@@ -0,0 +1,37 @@
+package com.devsaif.category.service.controller;
+
+import com.devsaif.category.service.model.Category;
+import com.devsaif.category.service.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/salon/{id}")
+    public ResponseEntity<Set<Category>> getCategoriesBySalon(@PathVariable Long id) {
+        Set<Category> categories = categoryService.getAllCategoriesBySalonId(id);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) throws Exception {
+        Category categories = categoryService.getCategoryById(id);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+
+
+}
---
+++ category-service/src/main/java/com/devsaif/category/service/controller/SalonCategoryController.java
@@ -0,0 +1,4 @@
+package com.devsaif.category.service.controller;
+
+public class SalonCategoryController {
+}
