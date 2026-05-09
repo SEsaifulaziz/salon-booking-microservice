d7604a63ea1053ac709c0c22459004def9fac434
SaifulAziz <gamestopik712@gmail.com>
2026-05-09 14:50:17 +0500

    Add ServiceOfferingService interface for implimentation

--- service-offering/src/main/java/com/devsaif/service/offering/service/ServiceOfferingService.java
+++ service-offering/src/main/java/com/devsaif/service/offering/service/ServiceOfferingService.java
@@ -1,4 +1,24 @@
 package com.devsaif.service.offering.service;
 
+
+import com.devsaif.service.offering.dto.CategoryDTO;
+import com.devsaif.service.offering.dto.SalonDTO;
+import com.devsaif.service.offering.dto.ServiceDTO;
+import com.devsaif.service.offering.model.ServiceOffering;
+
+import java.util.Set;
+
 public interface ServiceOfferingService {
+
+    ServiceOffering create(SalonDTO salondto, ServiceDTO servicedto, CategoryDTO categoryDto);
+
+    ServiceOffering update(Long serviceId, ServiceOffering service);
+
+    Set<ServiceOffering> getAllServiceBySalonId(Long serviceId, Long categoryId);
+
+    void deleteById(Long id);
+
+    Set<ServiceOffering> getServiceByIds(Set<Long> ids);
+
+
 }
