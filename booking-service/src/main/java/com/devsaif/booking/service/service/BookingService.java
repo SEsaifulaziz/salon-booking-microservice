9b7a53b48578ebfb7f1a45b7b411ddd1f1cb150c
SaifulAziz <gamestopik712@gmail.com>
2026-05-11 22:21:18 +0500

    Add BookingService interface

--- booking-service/src/main/java/com/devsaif/booking/service/service/BookingService.java
+++ booking-service/src/main/java/com/devsaif/booking/service/service/BookingService.java
@@ -1,4 +1,28 @@
 package com.devsaif.booking.service.service;
 
-public class BookingService {
+
+import com.devsaif.booking.service.domain.BookingStatus;
+import com.devsaif.booking.service.dto.BookingRequest;
+import com.devsaif.booking.service.dto.SalonDTO;
+import com.devsaif.booking.service.dto.ServiceDTO;
+import com.devsaif.booking.service.dto.UserDTO;
+import com.devsaif.booking.service.model.Booking;
+import com.devsaif.booking.service.model.SalonReport;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+public interface BookingService {
+
+    Booking createBooking(BookingRequest booking, UserDTO userDTO,
+                          SalonDTO salonDTO,
+                          Set<ServiceDTO> services);
+
+    List<Booking> getBookingsByCustomerId(Long customerId);
+    List<Booking> getBookingBySalonId(Long salonId);
+    Booking getBookingById(Long id);
+    Booking updateBooking(Long id, BookingStatus bookingStatus);
+    List<Booking> getBookingsByDate(LocalDate date, Long salonId);
+    SalonReport getSalonReport(Long salonId);
 }
