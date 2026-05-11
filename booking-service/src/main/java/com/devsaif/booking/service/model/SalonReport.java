f82ab9eb1900d8e71708dd3d7eb1aa8e85c4a5f2
SaifulAziz <gamestopik712@gmail.com>
2026-05-11 22:13:07 +0500

    Add SalonReport Entity for report

---
+++ booking-service/src/main/java/com/devsaif/booking/service/model/SalonReport.java
@@ -0,0 +1,15 @@
+package com.devsaif.booking.service.model;
+
+import lombok.Data;
+
+@Data
+public class SalonReport {
+
+
+    private Long salonId;
+    private String salonName;
+    private Double totalEarnings;
+    private Integer totalBookings;
+    private Integer cancelledBookings;
+    private Double totalRefund;
+}
