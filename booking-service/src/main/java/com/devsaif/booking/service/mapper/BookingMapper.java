71e52859563ceeeb22b0147c684c2985b1372298
SaifulAziz <gamestopik712@gmail.com>
2026-05-12 23:59:34 +0500

    Add mapper for DTO

---
+++ booking-service/src/main/java/com/devsaif/booking/service/mapper/BookingMapper.java
@@ -0,0 +1,21 @@
+package com.devsaif.booking.service.mapper;
+
+import com.devsaif.booking.service.dto.BookingDTO;
+import com.devsaif.booking.service.model.Booking;
+
+public class BookingMapper {
+
+    public static BookingDTO toDTO(Booking booking) {
+        BookingDTO bookingDTO = new BookingDTO();
+
+        bookingDTO.setId(booking.getId());
+        bookingDTO.setCustomerId(booking.getCustomerId());
+        bookingDTO.setStatus(booking.getStatus());
+        bookingDTO.setStartTime(booking.getStartTime());
+        bookingDTO.setEndTime(booking.getEndTime());
+        bookingDTO.setServiceIds(booking.getServiceIds());
+        bookingDTO.setSalonId(booking.getSalonId());
+        return bookingDTO;
+
+    }
+}
