376cdeecc51447eac19e82cb6c1b683428088135
SaifulAziz <gamestopik712@gmail.com>
2026-05-14 23:50:14 +0500

    Add DTO for slot

---
+++ booking-service/src/main/java/com/devsaif/booking/service/dto/SlotDto.java
@@ -0,0 +1,10 @@
+package com.devsaif.booking.service.dto;
+
+import java.time.LocalDateTime;
+
+public class SlotDto {
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+}
