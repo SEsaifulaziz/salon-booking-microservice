d86a0b854a0a6c4652a402bfa0e3ce6420905ae6
SaifulAziz <gamestopik712@gmail.com>
2026-05-14 17:32:51 +0500

    Add endpoint for getBookingsByCustomerId

--- booking-service/src/main/java/com/devsaif/booking/service/controller/BookingController.java
+++ booking-service/src/main/java/com/devsaif/booking/service/controller/BookingController.java
@@ -1,9 +1,7 @@
 package com.devsaif.booking.service.controller;
 
-import com.devsaif.booking.service.dto.BookingRequest;
-import com.devsaif.booking.service.dto.SalonDTO;
-import com.devsaif.booking.service.dto.ServiceDTO;
-import com.devsaif.booking.service.dto.UserDTO;
+import com.devsaif.booking.service.dto.*;
+import com.devsaif.booking.service.mapper.BookingMapper;
 import com.devsaif.booking.service.model.Booking;
 import com.devsaif.booking.service.service.BookingService;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/bookings")
@@ -46,6 +46,24 @@ public class BookingController {
                 serviceDTOSet);
 
         return ResponseEntity.ok().body(booking);
+    }
+
+
+    @GetMapping("/customer/")
+    public ResponseEntity<Set<BookingDTO>> getBookingsByCustomer(
+            @RequestParam Long salonId,
+            @RequestParam Long customerId
+    ){
+        List<Booking> bookings = bookingService.getBookingsByCustomerId(1L);
+
+        return ResponseEntity.ok(getBookingDTOs(bookings));
+    }
+
+    private Set<BookingDTO> getBookingDTOs(List<Booking> bookingSet){
+        return bookingSet.stream().map(booking ->  {
+            return BookingMapper.toDTO(booking);
+        }).collect(Collectors.toSet());
+
     }
 
 }
