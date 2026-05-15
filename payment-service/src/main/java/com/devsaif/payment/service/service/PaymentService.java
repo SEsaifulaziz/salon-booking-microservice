11adaaa344ace02b7bf8adedddaa0a11411f10b8
SaifulAziz <gamestopik712@gmail.com>
2026-05-15 16:22:52 +0500

    Add interface for PaymentSErvice

--- payment-service/src/main/java/com/devsaif/payment/service/service/PaymentService.java
+++ payment-service/src/main/java/com/devsaif/payment/service/service/PaymentService.java
@@ -1,4 +1,28 @@
 package com.devsaif.payment.service.service;
 
+import com.devsaif.payment.service.domain.PaymentMethod;
+import com.devsaif.payment.service.model.PaymentOrder;
+import com.devsaif.payment.service.payload.dto.BookingDTO;
+import com.devsaif.payment.service.payload.dto.UserDTO;
+import com.devsaif.payment.service.payload.response.PaymentLinkResponse;
+import com.stripe.model.PaymentLink;
+
 public interface PaymentService {
+
+    PaymentLinkResponse createOrder(UserDTO userDTO,
+                                    BookingDTO bookingDTO,
+                                    PaymentMethod paymentMethod);
+
+    PaymentOrder gePaymentOrderById(Long id);
+
+    PaymentOrder getPaymentByPaymentId(String paymentId);
+
+    PaymentLink createJazzCashPaymentLink(UserDTO user,
+                                          Long amount,
+                                          Long orderId);
+
+    String createStripePaymentLink(UserDTO userDTO,
+                                   Long amount,
+                                   Long orderId);
+
 }
