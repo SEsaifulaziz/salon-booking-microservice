ac0a00ea57e68ab04a60954974ab949e1f711df5
SaifulAziz <gamestopik712@gmail.com>
2026-05-16 00:00:03 +0500

    feat: add payment controller with create payment endpoint

---
+++ payment-service/src/main/java/com/devsaif/payment/service/controller/PaymentController.java
@@ -0,0 +1,36 @@
+package com.devsaif.payment.service.controller;
+
+
+import com.devsaif.payment.service.domain.PaymentMethod;
+import com.devsaif.payment.service.payload.dto.BookingDTO;
+import com.devsaif.payment.service.payload.dto.UserDTO;
+import com.devsaif.payment.service.payload.response.PaymentLinkResponse;
+import com.devsaif.payment.service.service.PaymentService;
+import com.stripe.exception.StripeException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<PaymentLinkResponse> createPaymentLink(
+            @RequestBody BookingDTO bookingDTO,
+            @RequestParam PaymentMethod paymentMethode
+            ) throws StripeException {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setFullName("Saifulaziz");
+        userDTO.setEmail("saifulazizse@gmail.com");
+        userDTO.setId(1L);
+
+        PaymentLinkResponse  response = paymentService.createOrder(userDTO, bookingDTO, paymentMethode);
+
+        return ResponseEntity.ok(response);
+
+    }
+}
