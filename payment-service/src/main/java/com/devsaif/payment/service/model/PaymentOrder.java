2a25dcd89f939bf73297b1bba9b13718e8d4c5f8
SaifulAziz <gamestopik712@gmail.com>
2026-05-15 14:56:41 +0500

    Add Payment Entity

--- payment-service/src/main/java/com/devsaif/payment/service/model/PaymentOrder.java
+++ payment-service/src/main/java/com/devsaif/payment/service/model/PaymentOrder.java
@@ -1,4 +1,33 @@
 package com.devsaif.payment.service.model;
 
+import com.devsaif.payment.service.domain.PaymentMethod;
+import com.devsaif.payment.service.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class PaymentOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private PaymentOrderStatus status = PaymentOrderStatus.PENDING;
+
+    @Column(nullable = false)
+    private PaymentMethod  paymentMethod;
+
+    private String paymentLinkedId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long salonId;
+
 }
