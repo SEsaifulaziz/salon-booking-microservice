2b4b9ad26c985a886d54865db2a16ea66ca4e315
SaifulAziz <gamestopik712@gmail.com>
2026-05-17 01:26:46 +0500

    Initial commit: Add gateway server

---
+++ gateway-server/src/test/java/com/devsaif/gateway/server/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsaif.gateway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
