ccf440f99888fef87a4448a9fb78963bb597d8ba
Saiful Aziz <gamestopik712@gmail.com>
2026-06-06 20:38:36 +0500

    feat: implement Keycloak token generation

--- user-service/src/main/java/com/devsaif/service/KeycloakService.java
+++ user-service/src/main/java/com/devsaif/service/KeycloakService.java
@@ -7,5 +7,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class KeycloakService {
 
+    private static final String KEYCLOAK_BASE_URL = "http://localhost:8080";
+    private static final String KEYCLOAK_ADMIN_API = KEYCLOAK_BASE_URL + "/admin/realms/master/users";
+
+    private static final String TOKEN_URL = KEYCLOAK_BASE_URL + "realms/master/protocol/openid-connect/token";
+
+
+
 
 }
