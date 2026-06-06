ff3c4fe065534b32c08dae3274a56662c341379e
Saiful Aziz <gamestopik712@gmail.com>
2026-06-06 21:51:11 +0500

    feat: create signup field

--- user-service/src/main/java/com/devsaif/payload/response/dto/SignupDTO.java
+++ user-service/src/main/java/com/devsaif/payload/response/dto/SignupDTO.java
@@ -1,4 +1,11 @@
 package com.devsaif.payload.response.dto;
 
 public class SignupDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+
 }
