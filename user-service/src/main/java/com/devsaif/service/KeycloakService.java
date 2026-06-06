6e81c991f1259c42f7ba0ec9d6568c4f5f3ca13f
Saiful Aziz <gamestopik712@gmail.com>
2026-06-06 20:39:24 +0500

    feat: add Keycloak authentication flow

--- user-service/src/main/java/com/devsaif/service/KeycloakService.java
+++ user-service/src/main/java/com/devsaif/service/KeycloakService.java
@@ -12,6 +12,13 @@ public class KeycloakService {
 
     private static final String TOKEN_URL = KEYCLOAK_BASE_URL + "realms/master/protocol/openid-connect/token";
 
+    private static final String CLIENT_ID = "salon-booking-client";
+    private static final String CLIENT_SECRET = "PnA7UBHoUmP5mK7u7UKhA2zaw8j7FL14";
+    private static final String GRANT_TYPE = "password";
+    private static final String scope = "openid profile email";
+    private static final String username = "saifulaziz";
+    private static final String password = "topik712";
+    private static final String clientId  = "29047eb7-6229-4f3a-b614-3d6b6d1bdd49";
 
 
 
