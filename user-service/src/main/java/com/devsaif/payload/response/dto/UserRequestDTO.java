438f71b8a93d545ca392c6fba5d19b8a37e136d8
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 01:20:17 +0500

    feat: add userRequsetDTO fields

--- user-service/src/main/java/com/devsaif/payload/response/dto/UserRequestDTO.java
+++ user-service/src/main/java/com/devsaif/payload/response/dto/UserRequestDTO.java
@@ -1,4 +1,17 @@
 package com.devsaif.payload.response.dto;
 
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class UserRequestDTO {
+
+    private String username;
+    private Boolean enabled;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private List<CredentialDTO> credentials = new ArrayList<>();
 }
