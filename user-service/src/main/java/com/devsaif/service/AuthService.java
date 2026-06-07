6bc1cebce475227f058f3fb88f181c52af1f6721
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 16:12:14 +0500

    chore: add exceptions

--- user-service/src/main/java/com/devsaif/service/AuthService.java
+++ user-service/src/main/java/com/devsaif/service/AuthService.java
@@ -5,7 +5,7 @@ import com.devsaif.payload.dto.SignupDTO;
 import com.devsaif.payload.response.AuthResponse;
 
 public interface AuthService {
-    AuthResponse login(String username, String password);
+    AuthResponse login(String username, String password) throws Exception;
     AuthResponse signup(SignupDTO req) throws Exception;
-    AuthResponse getAccessTokenFromRefreshToken(String refreshToken);
+    AuthResponse getAccessTokenFromRefreshToken(String refreshToken) throws Exception;
 }
