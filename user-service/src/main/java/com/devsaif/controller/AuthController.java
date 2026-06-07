54da513e8246c65542c97308af7b6dbee61441af
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 16:29:19 +0500

    feat(auth): implement user login endpoint in AuthController

--- user-service/src/main/java/com/devsaif/controller/AuthController.java
+++ user-service/src/main/java/com/devsaif/controller/AuthController.java
@@ -1,14 +1,13 @@
 package com.devsaif.controller;
 
+import com.devsaif.payload.dto.LoginDTO;
 import com.devsaif.payload.dto.SignupDTO;
 import com.devsaif.payload.response.AuthResponse;
 import com.devsaif.service.AuthService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +21,22 @@ public class AuthController {
             @RequestBody SignupDTO req ) throws Exception{
 
         AuthResponse authResponse = authService.signup(req);
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginDTO req ) throws Exception {
+
+        AuthResponse authResponse = authService.login(req.getUsername(), req.getPassword());
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @GetMapping("/access-token/refresh-token/{refreshToken}")
+    public ResponseEntity<AuthResponse> getAccessToken(
+            @PathVariable String refreshToken ) throws Exception {
+
+        AuthResponse authResponse = authService.getAccessTokenFromRefreshToken(refreshToken);
         return ResponseEntity.ok(authResponse);
     }
 }
