63bea68f1fe8ebfef31b80e37c88dce0c6418a68
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 16:05:32 +0500

    feat: implement signup method for new signup

--- user-service/src/main/java/com/devsaif/service/impl/AuthServiceImpl.java
+++ user-service/src/main/java/com/devsaif/service/impl/AuthServiceImpl.java
@@ -1,4 +1,60 @@
 package com.devsaif.service.impl;
 
-public class AuthServiceImpl {
+import com.devsaif.model.User;
+import com.devsaif.payload.dto.SignupDTO;
+import com.devsaif.payload.response.AuthResponse;
+import com.devsaif.payload.response.TokenResponse;
+import com.devsaif.repository.UserRepository;
+import com.devsaif.service.AuthService;
+import com.devsaif.service.KeycloakService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepo;
+    private final KeycloakService keycloakService;
+
+    @Override
+    public AuthResponse login(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public AuthResponse signup(SignupDTO req) throws Exception {
+        keycloakService.createUser(req);
+
+        User user = new User();
+        user.setUserName(req.getUsername());
+        user.setPassword(req.getPassword());
+        user.setEmail(req.getEmail());
+        user.setRole(req.getRole());
+        user.setFullName(req.getFirstName() + " " + req.getLastName());
+        user.setCreatedAt(LocalDateTime.now());
+        userRepo.save(user);
+
+        TokenResponse tokenResponse = keycloakService.getAdminAccessToken(
+                req.getUsername(),
+                req.getPassword(),
+                "password",
+                null
+        );
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setRefresh_token(tokenResponse.getRefreshToken());
+        authResponse.setJwt(tokenResponse.getAccessToken());
+        authResponse.setRole(user.getRole());
+        authResponse.setMessage("Registered Successfully");
+
+        return authResponse;
+    }
+
+    @Override
+    public AuthResponse getAccessTokenFromRefreshToken(String refreshToken) {
+        return null;
+    }
 }
