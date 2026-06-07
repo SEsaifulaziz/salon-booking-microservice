53b4b5022b5f5038983cc67c923203b7c9f5f1e7
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 11:41:36 +0500

    feat: add keycloak user DTO fields

--- user-service/src/main/java/com/devsaif/payload/response/dto/KeycloakUserDTO.java
+++ user-service/src/main/java/com/devsaif/payload/response/dto/KeycloakUserDTO.java
@@ -1,5 +1,12 @@
 package com.devsaif.payload.response.dto;
 
-public class KeycloakUserDTO {
+import lombok.Data;
 
+@Data
+public class KeycloakUserDTO {
+    private String id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
 }
