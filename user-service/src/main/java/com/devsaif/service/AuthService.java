55d9b56b64c3f5b7aaa450fb082e03c9ad478a7f
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 15:31:04 +0500

    feat: add AuthService interface methods

--- user-service/src/main/java/com/devsaif/service/AuthService.java
+++ user-service/src/main/java/com/devsaif/service/AuthService.java
@@ -1,5 +1,11 @@
 package com.devsaif.service;
 
 
-public class AuthService {
+import com.devsaif.payload.dto.SignupDTO;
+import com.devsaif.payload.response.AuthResponse;
+
+public interface AuthService {
+    AuthResponse login(String username, String password);
+    AuthResponse signup(SignupDTO req);
+    AuthResponse getAccessTokenFromRefreshToken(String refreshToken);
 }
