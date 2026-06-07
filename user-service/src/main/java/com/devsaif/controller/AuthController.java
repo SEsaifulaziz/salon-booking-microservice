3d70bb20f37d6e91576b9f3e84b70eca838193f7
Saiful Aziz <gamestopik712@gmail.com>
2026-06-07 16:24:41 +0500

    feat: implement user signup endpoint in AuthController

--- user-service/src/main/java/com/devsaif/controller/AuthController.java
+++ user-service/src/main/java/com/devsaif/controller/AuthController.java
@@ -1,6 +1,12 @@
 package com.devsaif.controller;
 
+import com.devsaif.payload.dto.SignupDTO;
+import com.devsaif.payload.response.AuthResponse;
+import com.devsaif.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(
+            @RequestBody SignupDTO req ) throws Exception{
+
+        AuthResponse authResponse = authService.signup(req);
+        return ResponseEntity.ok(authResponse);
+    }
 }
