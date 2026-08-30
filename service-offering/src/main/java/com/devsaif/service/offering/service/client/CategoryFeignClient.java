1588d6bdbebb5d2e1ce56f0847d4877400f10465
Saiful Aziz <gamestopik712@gmail.com>
2026-08-30 16:44:59 +0500

    feat(service-offering): add CategoryFeignClient to resolve category by id

--- service-offering/src/main/java/com/devsaif/service/offering/service/client/CategoryFeignClient.java
+++ service-offering/src/main/java/com/devsaif/service/offering/service/client/CategoryFeignClient.java
@@ -1,4 +1,18 @@
 package com.devsaif.service.offering.service.client;
 
-public class CategoryFiegnClient {
+import com.devsaif.service.offering.dto.CategoryDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("CATEGORY-SERVICE")
+public interface CategoryFeignClient {
+
+    @GetMapping("/api/categories/getById/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(
+            @PathVariable Long id
+    ) throws Exception;
+
+
 }
