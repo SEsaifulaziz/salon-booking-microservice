baf4c13acd7cf3051218fcf1e5ae1b0664d792e0
Saiful Aziz <gamestopik712@gmail.com>
2026-08-30 16:43:58 +0500

    feat(service-offering): add SalonFeignClient to resolve owner's salon

--- service-offering/src/main/java/com/devsaif/service/offering/service/client/SalonFeignClient.java
+++ service-offering/src/main/java/com/devsaif/service/offering/service/client/SalonFeignClient.java
@@ -1,4 +1,17 @@
 package com.devsaif.service.offering.service.client;
 
+
+import com.devsaif.service.offering.dto.SalonDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient("SALON-SERVICE")
 public interface SalonFeignClient {
+
+    @GetMapping("/api/salons/owner")
+    public ResponseEntity<SalonDTO> getSalonByOwnerId(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception;
 }
