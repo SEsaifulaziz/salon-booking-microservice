8c008848b0981ef50bcb2ef90695bfed9a55fc93
Saiful Aziz <gamestopik712@gmail.com>
2026-09-01 23:31:17 +0500

    feat: add categoryDTO

--- category-service/src/main/java/com/devsaif/category/service/dto/CategoryDto.java
+++ category-service/src/main/java/com/devsaif/category/service/dto/CategoryDto.java
@@ -1,4 +1,11 @@
 package com.devsaif.category.service.dto;
 
+import lombok.Data;
+
+@Data
 public class CategoryDto {
+
+    private Long id;
+    private String name;
+    private String image;
 }
