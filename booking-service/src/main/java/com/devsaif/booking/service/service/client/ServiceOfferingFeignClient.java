bb0ffdf20c61df793d9ae62afa14b31999592dd1
Saiful Aziz <gamestopik712@gmail.com>
2026-09-03 11:19:15 +0500

    feat(booking): add ServiceOfferingFeignlient to resolve booked services by id

--- booking-service/src/main/java/com/devsaif/booking/service/service/client/ServiceOfferingFeignClient.java
+++ booking-service/src/main/java/com/devsaif/booking/service/service/client/ServiceOfferingFeignClient.java
@@ -1,4 +1,19 @@
 package com.devsaif.booking.service.service.client;
 
-public class ServiceOfferingFeignClient {
+import com.devsaif.booking.service.dto.ServiceDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Set;
+
+@FeignClient("SERVICE-OFFERING")
+public interface ServiceOfferingFeignClient {
+
+
+    @GetMapping("/api/service-offering/List/{ids}")
+    public ResponseEntity<Set<ServiceDTO>> getServiceByIds(
+            @PathVariable Set<Long> ids
+    );
 }
