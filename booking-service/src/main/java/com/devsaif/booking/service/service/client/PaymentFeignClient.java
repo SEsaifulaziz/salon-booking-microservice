a0d07f0c4d404059f3503a59763ac1fc74392a7e
Saiful Aziz <gamestopik712@gmail.com>
2026-09-03 11:18:15 +0500

    feat(booking): add PaymentFeignClient to request a payment link on booking creation

--- booking-service/src/main/java/com/devsaif/booking/service/service/client/PaymentFeignClient.java
+++ booking-service/src/main/java/com/devsaif/booking/service/service/client/PaymentFeignClient.java
@@ -1,4 +1,22 @@
 package com.devsaif.booking.service.service.client;
 
-public interface paymentFeignClient {
+import com.devsaif.booking.service.domain.PaymentMethod;
+import com.devsaif.booking.service.dto.BookingDTO;
+import com.devsaif.booking.service.dto.PaymentLinkResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("PAYMENT-SERVICE")
+public interface PaymentFeignClient {
+
+    @PostMapping("/api/payment/create")
+    public ResponseEntity<PaymentLinkResponse> createPaymentLink(
+            @RequestBody BookingDTO bookingDTO,
+            @RequestParam PaymentMethod paymentMethode,
+            @RequestHeader("Authorization")  String jwt
+    );
 }
