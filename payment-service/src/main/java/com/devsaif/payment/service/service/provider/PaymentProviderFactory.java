2f50b6a42d5655b2ef4439d8232e9e22a0792356
Saiful Aziz <gamestopik712@gmail.com>
2026-09-03 14:55:58 +0500

    feat(payment): add payment provider factory

---
+++ payment-service/src/main/java/com/devsaif/payment/service/service/provider/PaymentProviderFactory.java
@@ -0,0 +1,38 @@
+package com.devsaif.payment.service.service.provider;
+
+import com.devsaif.payment.service.domain.PaymentMethod;
+import org.springframework.stereotype.Component;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+@Component
+public class PaymentProviderFactory {
+
+    private final Map<PaymentMethod, PaymentProvider> providers;
+
+    public PaymentProviderFactory(
+            StripePaymentProvider stripePaymentProvider
+    ) {
+        this.providers = new EnumMap<>(PaymentMethod.class);
+
+        providers.put(
+                PaymentMethod.STRIPE,
+                stripePaymentProvider
+        );
+    }
+
+    public PaymentProvider getProvider(PaymentMethod paymentMethod) {
+
+        PaymentProvider provider = providers.get(paymentMethod);
+
+        if (provider == null) {
+            throw  new IllegalArgumentException(
+                    "No payment provider configured for: " + paymentMethod
+            );
+        }
+
+        return provider;
+    }
+
+}
