a9d6bfa88b7607e66b2dc04a866d28d8ec5ba272
Saiful Aziz <gamestopik712@gmail.com>
2026-09-04 22:25:14 +0500

    feat(notifications): add notification repository

--- notifications/src/main/java/com/devsaif/notifications/repository/NotificationRepository.java
+++ notifications/src/main/java/com/devsaif/notifications/repository/NotificationRepository.java
@@ -1,4 +1,15 @@
 package com.devsaif.notifications.repository;
 
-public class NotificationRepository {
+import com.devsaif.notifications.model.NotificationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+
+    List<NotificationEntity> findByUserId(Long userId);
+    List<NotificationEntity> findBySalonId(Long  salonId);
+
 }
