2dba18d92428168efe656e3bce5ab108e8068e44
Saiful Aziz <gamestopik712@gmail.com>
2026-09-04 22:26:48 +0500

    feat(notifications): add notification service implementation

--- notifications/src/main/java/com/devsaif/notifications/service/impl/NotificationServiceImpl.java
+++ notifications/src/main/java/com/devsaif/notifications/service/impl/NotificationServiceImpl.java
@@ -1,4 +1,38 @@
 package com.devsaif.notifications.service.impl;
 
-public class NotificationServiceImpl {
+import com.devsaif.notifications.model.NotificationEntity;
+import com.devsaif.notifications.payload.dto.NotificationDTO;
+import com.devsaif.notifications.repository.NotificationRepository;
+import com.devsaif.notifications.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+
+    @Override
+    public NotificationDTO createNotification(NotificationDTO notificationDTO) {
+        return null;
+    }
+
+    @Override
+    public List<NotificationEntity> getAllNotificationsByUserId(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<NotificationEntity> getAllNotificationsBySalonId(Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public NotificationEntity markNotificationAsRead(Long notificationId) {
+        return null;
+    }
 }
