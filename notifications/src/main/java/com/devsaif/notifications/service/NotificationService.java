b1a1daa49f5354d6da2a975661fc58e6779aa357
Saiful Aziz <gamestopik712@gmail.com>
2026-09-04 22:25:43 +0500

    feat(notifications): add notification service interface

--- notifications/src/main/java/com/devsaif/notifications/service/NotificationService.java
+++ notifications/src/main/java/com/devsaif/notifications/service/NotificationService.java
@@ -1,4 +1,18 @@
 package com.devsaif.notifications.service;
 
-public class NotificationService {
+import com.devsaif.notifications.model.NotificationEntity;
+import com.devsaif.notifications.payload.dto.NotificationDTO;
+
+import java.util.List;
+
+
+public interface NotificationService {
+
+    NotificationDTO createNotification(NotificationDTO notificationDTO);
+
+    List<NotificationEntity> getAllNotificationsByUserId(Long userId);
+    List<NotificationEntity> getAllNotificationsBySalonId(Long salonId);
+
+    NotificationEntity markNotificationAsRead(Long notificationId);
+
 }
