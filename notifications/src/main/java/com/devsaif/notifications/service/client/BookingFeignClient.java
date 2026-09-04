bf7d9a7b6f33b0cdf6075c7e96b52e81f3303062
Saiful Aziz <gamestopik712@gmail.com>
2026-09-04 22:26:08 +0500

    feat(notifications): add booking feign client

--- notifications/src/main/java/com/devsaif/notifications/service/client/BookingFeignClient.java
+++ notifications/src/main/java/com/devsaif/notifications/service/client/BookingFeignClient.java
@@ -1,4 +1,18 @@
 package com.devsaif.notifications.service.client;
 
-public class BookingFeignClient {
+import com.devsaif.notifications.payload.dto.BookingDTO;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@EnableFeignClients("BOOKING-SERVICE")
+public interface BookingFeignClient {
+
+    @GetMapping("api/bookings/id/{bookingId}")
+    public ResponseEntity<BookingDTO> getBookingsByBookingId(
+            @PathVariable Long bookingId
+    ) throws Exception;
+
+
 }
