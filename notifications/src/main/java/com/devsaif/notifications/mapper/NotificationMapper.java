071ab9a368fb19114c118e212b5b05a114a84684
Saiful Aziz <gamestopik712@gmail.com>
2026-09-05 23:02:57 +0500

    feat(notifications): add notifications mapper

--- notifications/src/main/java/com/devsaif/notifications/mapper/NotificationMapper.java
+++ notifications/src/main/java/com/devsaif/notifications/mapper/NotificationMapper.java
@@ -1,4 +1,26 @@
 package com.devsaif.notifications.mapper;
 
+
+import com.devsaif.notifications.model.Notification;
+import com.devsaif.notifications.payload.dto.BookingDTO;
+import com.devsaif.notifications.payload.dto.NotificationDTO;
+
 public class NotificationMapper {
+
+    public static NotificationDTO toDTO(Notification notification, BookingDTO bookingDTO) {
+
+        NotificationDTO notificationDTO = new NotificationDTO();
+        notificationDTO.setId(notification.getId());
+        notificationDTO.setIsRead(notification.getIsRead());
+        notificationDTO.setDescription(notification.getDescription());
+        notificationDTO.setType(notification.getType());
+        notificationDTO.setBookingId(bookingDTO.getId());
+        notificationDTO.setUserId(notification.getUserId());
+        notificationDTO.setSalonId(notification.getSalonId());
+        notificationDTO.setCreatedAt(notification.getCreatedAt());
+
+        return notificationDTO;
+
+    }
+
 }
