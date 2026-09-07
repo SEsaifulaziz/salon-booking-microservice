270fda5f0e6e88cb92da81c4aa2f80aaf58b93d6
Saiful Aziz <gamestopik712@gmail.com>
2026-09-07 23:48:52 +0500

    feat(notifications): bind payment events queue

--- notifications/src/main/java/com/devsaif/notifications/configuration/RabbitMQConfig.java
+++ notifications/src/main/java/com/devsaif/notifications/configuration/RabbitMQConfig.java
@@ -1,33 +1,50 @@
 package com.devsaif.notifications.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 @Configuration
 public class RabbitMQConfig {
 
+    public static final String PAYMENT_EVENTS_EXCHANGE = "payment.events";
+        public static final String NOTIFICATION_QUEUE = "notification.payment-successful.queue";
+
     @Bean
-    public JacksonJsonMessageConverter jackson2JsonMessageConverter(){
+    public FanoutExchange paymentEventsExchange() {
+        return new FanoutExchange(PAYMENT_EVENTS_EXCHANGE);
+    }
+
+    @Bean
+    public Queue bookingPaymentSuccessfulQueue() {
+        return new Queue(NOTIFICATION_QUEUE, true);
+    }
+
+    @Bean
+    public Binding bookingQueueBinding(Queue bookingPaymentSuccessfulQueue,
+                                       FanoutExchange paymentEventsExchange) {
+        return BindingBuilder.bind(bookingPaymentSuccessfulQueue).to(paymentEventsExchange);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
         return new JacksonJsonMessageConverter();
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
             ConnectionFactory connectionFactory,
-            JacksonJsonMessageConverter jacksonJsonMessageConverter
-    ) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jacksonJsonMessageConverter);
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public Queue notificationQueue() {
-        return new Queue("notification-queue");
+            MessageConverter jsonMessageConverter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter);
+        return factory;
     }
 }
