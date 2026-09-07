bbf278b146fe77caa8de570edb03a754d65b11e7
Saiful Aziz <gamestopik712@gmail.com>
2026-09-07 16:50:12 +0500

    feat(payment): configre payment events exchange

--- payment-service/src/main/java/com/devsaif/payment/service/messaging/PaymentSuccessfulEvent.java
+++ payment-service/src/main/java/com/devsaif/payment/service/messaging/PaymentSuccessfulEvent.java
@@ -1,4 +1,19 @@
 package com.devsaif.payment.service.messaging;
 
-public class PaymentSuccessfulEvent {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentSuccessfulEvent implements Serializable {
+
+    private Long bookingId;
+    private Long paymentOrderId;
+    private Long userId;
+    private Long salonId;
+    private Long amount;
 }
