ce83425776d159a903a7d943b6acab69ea5486e8
Saiful Aziz <gamestopik712@gmail.com>
2026-09-07 16:52:06 +0500

    feat(payment): configure payment events exchange

--- payment-service/src/main/java/com/devsaif/payment/service/config/RabbitMQConfig.java
+++ payment-service/src/main/java/com/devsaif/payment/service/config/RabbitMQConfig.java
@@ -1,4 +1,33 @@
 package com.devsaif.payment.service.config;
 
-public class RAbbitMqConfig {
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String PAYMENT_EVENTS_EXCHANGE = "payment.events";
+
+    @Bean
+    public FanoutExchange paymentEventsExchange() {
+        return new FanoutExchange(PAYMENT_EVENTS_EXCHANGE);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter jsonMessageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter);
+        return template;
+    }
 }
