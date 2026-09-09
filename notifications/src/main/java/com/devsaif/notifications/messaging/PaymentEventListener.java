f526323c866f4303301bae3147aaacab6877b450
Saiful Aziz <gamestopik712@gmail.com>
2026-09-09 16:53:23 +0500

    fea(notifications): prevent duplicate paymet notifications

--- notifications/src/main/java/com/devsaif/notifications/messaging/PaymentEventListener.java
+++ notifications/src/main/java/com/devsaif/notifications/messaging/PaymentEventListener.java
@@ -3,6 +3,7 @@ package com.devsaif.notifications.messaging;
 
 import com.devsaif.notifications.configuration.RabbitMQConfig;
 import com.devsaif.notifications.model.Notification;
+import com.devsaif.notifications.repository.NotificationRepository;
 import com.devsaif.notifications.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,13 +15,25 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class PaymentEventListener {
 
+    private static final String PAYMENT_SUCCESSFUL = "PAYMENT_SUCCESSFUL";
+
     private final NotificationService notificationService;
+    private final NotificationRepository notificationRepository;
 
     @RabbitListener(queues = RabbitMQConfig.NOTIFICATION_QUEUE)
     public void onPaymentSuccessful(PaymentSuccessfulEvent event) throws Exception {
 
+        boolean alreadyExists = notificationRepository
+                .existsByPaymentOrderIdAndType(
+                        event.getPaymentOrderId(),
+                        PAYMENT_SUCCESSFUL
+                );
+        if(alreadyExists){
+            return;
+        }
+
             Notification notification = new Notification();
-            notification.setType("PAYMENT_SUCCESSFUL");
+            notification.setType(PAYMENT_SUCCESSFUL);
             notification.setDescription(
                     "Your payment was successful and your booking is confirmed."
             );
