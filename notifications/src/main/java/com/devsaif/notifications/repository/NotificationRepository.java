999a3b291b86d5954b56173ee9c3a8416809ab52
Saiful Aziz <gamestopik712@gmail.com>
2026-09-09 16:47:01 +0500

    fea(notifications): add payment notification idempotency query

--- notifications/src/main/java/com/devsaif/notifications/repository/NotificationRepository.java
+++ notifications/src/main/java/com/devsaif/notifications/repository/NotificationRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findByUserId(Long userId);
+
     List<Notification> findBySalonId(Long  salonId);
+
+    boolean existsByPaymentOrderIdAndType(Long paymentOrderId, String type);
 
 }
