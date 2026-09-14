edf1ed8c9be293faf81c6fbc786a3c9e37116fe3
Saiful Aziz <gamestopik712@gmail.com>
2026-09-14 12:38:27 +0500

    fix(notifications): include booking details in notification response

--- notifications/src/main/java/com/devsaif/notifications/mapper/NotificationMapper.java
+++ notifications/src/main/java/com/devsaif/notifications/mapper/NotificationMapper.java
@@ -7,17 +7,22 @@ import com.devsaif.notifications.payload.dto.NotificationDTO;
 
 public class NotificationMapper {
 
-    public static NotificationDTO toDTO(Notification notification, BookingDTO bookingDTO) {
+
+    public static NotificationDTO toDTO(
+            Notification notification,
+            BookingDTO bookingDTO) {
 
         NotificationDTO notificationDTO = new NotificationDTO();
+
         notificationDTO.setId(notification.getId());
         notificationDTO.setIsRead(notification.getIsRead());
         notificationDTO.setDescription(notification.getDescription());
         notificationDTO.setType(notification.getType());
-        notificationDTO.setBookingId(bookingDTO.getId());
+        notificationDTO.setBookingId(notification.getBookingId());
         notificationDTO.setUserId(notification.getUserId());
         notificationDTO.setSalonId(notification.getSalonId());
         notificationDTO.setCreatedAt(notification.getCreatedAt());
+        notificationDTO.setBooking(bookingDTO);
 
         return notificationDTO;
 
