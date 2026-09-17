f9775d271770cdbe974016bcbd6efec365ddec98
Saiful Aziz <gamestopik712@gmail.com>
2026-09-17 16:16:23 +0500

    feat(theme): add theme directory

---
+++ frontend/src/theme/greenTheme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const greenTheme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#019031'
+        },
+        legendary: {
+            main: '#019031'
+        }
+    }
+});
+
+export default greenTheme;
