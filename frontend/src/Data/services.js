77b893451b3a77754f90adefd091753d5608bc64
Saiful Aziz <gamestopik712@gmail.com>
2026-09-19 13:48:31 +0500

    feat: add home services data

---
+++ frontend/src/Data/services.js
@@ -0,0 +1,62 @@
+export const services = [
+
+    {
+        id: 1,
+        name: "Hair Cut",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7VPeYBlqBHHcVRNJW43M2Tq8X5edlPfpNtWkzWRJJbw&s"
+    },
+
+    {
+        id: 2,
+        name: "Manicure",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaT8jguF4yFGX_0-PGXfJ3-Qi-XYLtWla4vmNeoYwRjg&s=10"
+    },
+
+    {
+        id: 3,
+        name: "Pedicure",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPpNzH9HLbSeq2RLrlIyOYkWgoUiAfHzXGvmAC4Bm_D_u56vQjc17vHvtx&s=10"
+    },
+
+    {
+        id: 4,
+        name: "Massage Therapy",
+        image: "https://tattvaspa.com/wp-content/uploads/2025/04/hammam-spa-massage.jpg"
+    },
+
+    {
+        id: 5,
+        name: "Bridal Makeup",
+        image: "https://www.shaadidukaan.com/vogue/wp-content/uploads/2019/04/All-About-Surbhi-Chandna-and-Karan-Sharma-Wedding-Attire.png"
+    },
+
+    {
+        id: 6,
+        name: "Hair Coloring",
+        image: "https://media.istockphoto.com/id/1305824214/photo/woman-dyeing-her-hair-at-the-salon.jpg?s=612x612&w=0&k=20&c=Jk2XQqn-5Tf1IeUPhmLYMP1Lq2nSlW_0udRXzc_KAJI="
+    },
+
+    {
+        id: 7,
+        name: "Waxing",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGD5DU8JECJibBaEomJTPVCI5EvvqR3m-1sdwFjSHReyvIQxIf6LchS3U&s=10"
+    },
+
+    {
+        id: 8,
+        name: "Skin Whitening",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPlvIlKenIQVcXm0zl5KJGGAL_XigudzTIO6naFNQYuGbIJL1VPMc3oXfP&s=10"
+    },
+
+    {
+        id: 9,
+        name: "Hair Straightening",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDbBx1EOVXuX_vOXrjbAVLgHInwhqZ5eW6uwjr-XSSyS80pfrZFc2TmH3e&s=10"
+    },
+    {
+        id: 10,
+        name: "Facial Treatment",
+        image: "https://irp.cdn-website.com/3bf2f26c/dms3rep/multi/opt/shutterstock_2245240265-640h.jpg"
+    }
+
+]
