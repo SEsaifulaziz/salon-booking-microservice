6ed4c6d5054185260d04784490bbe36ed1701698
Saiful Aziz <gamestopik712@gmail.com>
2026-09-19 16:36:36 +0500

    feat: add salon details component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { ThemeProvider } from '@mui/material';
 import greenTheme from './theme/greenTheme';
 import Home from './Customer/Home/Home';
+import SalonDetails from './Salon/SalonDetails/SalonDetails';
 
 function App() {
   return (
     <ThemeProvider theme={greenTheme}>
-      <Home />
+      {/* <Home /> */}
+      <SalonDetails/>
     </ThemeProvider>
   );
 }
